b02e75862b941efb7e8206094aef9cc16321adf1
Denis Emanov <denemanow@yandex.ru>
2022-12-02 18:19:25 +0300

    fixes runaway pointer in memvga_cursor_virtual_newline

--- lib/memvga_cursor_virtual.asm
+++ lib/memvga_cursor_virtual.asm
@@ -16,7 +16,14 @@ memvga_cursor_virtual_newline:
         div bl
         shr ax, 8
         sub [memvga_cursor_virtual_coordinates], eax
-        add [memvga_cursor_virtual_coordinates], 160
+        cmp dword [memvga_cursor_virtual_coordinates], (80*24*2)  ; if coords point to the last line,
+        jae .last_line ; do scrolling
+        add dword [memvga_cursor_virtual_coordinates], 160 ; else get to the next line
+        jmp .end
+
+.last_line:
+        jmp .end
+.end:
         pop ebx
         pop eax
         ret
